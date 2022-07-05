03f39f37c7f52bbd6221896d8d15a81285beccce
pvvyas <108752808+pvvyas@users.noreply.github.com>
2022-07-06 02:30:59 +0530

    Create zero.jsx

---
+++ frontend/src/components/friends/zero.jsx
@@ -0,0 +1,10 @@
+import "./zero.css";
+
+export default function Zero(){
+    return(
+        <div className="zeros">
+        <i className="fas fa-exclamation-circle"></i>
+          You have No friends
+        </div>
+    )
+}
